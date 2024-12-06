8345ec6d08607d8bc5b82d0c20a2383221ba941b
RELAYKER <115099690+RELAYKER@users.noreply.github.com>
2024-12-05 19:42:40 -0600

    Delete js directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("crud-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    addData({ name, email });
-    e.target.reset();
-});
