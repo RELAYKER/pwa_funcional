4d47b4fb9ddcb081a24aa699f6ac50d7ed337ded
RELAYKER <115099690+RELAYKER@users.noreply.github.com>
2024-12-05 20:48:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -44,12 +44,12 @@ resetButton.addEventListener("click", () => {
 
 // Registrar el Service Worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js')  // Debe ser '/service-worker.js'
         .then((registration) => {
             console.log('Service Worker registrado con éxito:', registration);
         })
         .catch((error) => {
-            console.error('Error al registrar el Service Worker:', error);
+            console.log('Error al registrar el Service Worker:', error);
         });
 }
+
