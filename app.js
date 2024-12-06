5ae43236ff75e07a3312764762930fb3ad41165c
RELAYKER <115099690+RELAYKER@users.noreply.github.com>
2024-12-05 20:26:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/service-worker.js')
+        .register('./service-worker.js')
         .then(() => console.log('Service Worker registrado correctamente.'))
         .catch((err) => console.error('Error al registrar el Service Worker:', err));
 }
